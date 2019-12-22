6d3a0f53769b4fabb703ea54cc040c7d94e508df
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2019-12-21 23:07:28 -0500

    clean up of code

--- scripts/ups_installer.sh
+++ scripts/ups_installer.sh
@@ -6,8 +6,8 @@ declare -u os;
 wget -nd https://raw.githubusercontent.com/buyapi/ups/master/scripts/ups;
 wget -nd https://raw.githubusercontent.com/buyapi/ups/master/scripts/ups.service;
 
-chmod 0510 ups;
-chmod 0510 ups.service;
+chmod -f u=rx,g=rx,o=r ups;
+chmod -f u=rx,g=rx,o=r ups.service;
 
 chown -f root:root ups;
 chown -f root:root ups.service;
@@ -15,11 +15,11 @@ chown -f root:root ups.service;
 if [ "$os" = "LIBREELEC" ] || [ "$os" = "OPENELEC" ]
 then
         # these will run as normal, as the default user account is root
-        mkdir -p /storage/.kodi/addons/ups_power/bin;
-        mv -f ups /storage/.kodi/addons/ups_power/bin;
-        chmod 0555 /storage/.kodi/addons/ups_power/;
+        mkdir -p /storage/.kodi/addons/.ups_power/bin;
+        mv -f ups /storage/.kodi/addons/.ups_power/bin;
+        chmod u=rx,g=rx,o=rx /storage/.kodi/addons/.ups_power/;
   
-        sed -e "s:ExecStart=\/bin\/ups\/:ExecStart=\/storage\/.kodi\/addons\/ups_power\/bin:" ups.service
+        sed -e "s:ExecStart=\/bin\/ups\/:ExecStart=\/storage\/\.kodi\/addons\/\.ups_power\/bin:" ups.service
         mv -f ups.service /storage/.config/system.d/;
 else
         mv -f ups /bin/;
