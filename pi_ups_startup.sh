7008ae605f2b95fd6971b6d96c5ecf3d1b61098c
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2019-12-22 12:23:50 -0500

    tidy up of code

--- pi_ups_startup.sh
+++ pi_ups_startup.sh
@@ -11,15 +11,13 @@ then
     wget --non-verbose --tries=3 https://raw.githubusercontent.com/buyapi/ups/master/scripts/ups;
     chmod --changes u=rx,g=rx,o=r ups;
     chown --changes root:root ups;
-     
-    wget --non-verbose --tries=3 https://raw.githubusercontent.com/buyapi/ups/master/scripts/elec.ups.service;
-    chmod --changes u=rx,g=rx,o=r elec.ups.service;
-    chown --changes root:root elec.ups.service;
-    
     mkdir --parents /storage/.kodi/addons/.ups_power/bin/;
     mv --force ups /storage/.kodi/addons/.ups_power/bin/;
     chmod --changes u=rx,g=rx,o=rx /storage/.kodi/addons/.ups_power/;
-  
+    
+    wget --non-verbose --tries=3 https://raw.githubusercontent.com/buyapi/ups/master/scripts/elec.ups.service;
+    chmod --changes u=rx,g=rx,o=r elec.ups.service;
+    chown --changes root:root elec.ups.service;
     mv --force elec.ups.service /storage/.config/system.d/;
 elif [ ]
 then
@@ -28,11 +26,10 @@ else
     wget --non-verbose --tries=3 https://raw.githubusercontent.com/buyapi/ups/master/scripts/ups;
     chmod --changes u=rx,g=rx,o=r ups;
     chown --changes root:root ups;
+    mv --force ups /bin/;
     
     wget --non-verbose --tries=3 https://raw.githubusercontent.com/buyapi/ups/master/scripts/std_linux.ups.service;
     chmod --changes u=rx,g=rx,o=r std_linux.ups.service;
     chown --changes root:root std_linux.ups.service;
-
-    mv --force ups /bin/;
     mv --force std_linux.ups.service /etc/systemd/system/;
 fi
