c56646a9b88bf372a5e45788a68a7302ea63f702
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2019-12-22 01:06:11 -0500

    Update ups_installer.sh

--- scripts/ups_installer.sh
+++ scripts/ups_installer.sh
@@ -12,15 +12,19 @@ chmod --changes u=rx,g=rx,o=r ups.service;
 chown --changes root:root ups;
 chown --changes root:root ups.service;
 
-if [ "$os" = "LIBREELEC" ] || [ "$os" = "OPENELEC" ]
+if [ "$os" = "LIBREELEC" ] || [ "$os" = "OPENELEC" ] || [ "$os" = "OSMC" ]
 then
         # these will run as normal, as the default user account is root
+
         mkdir --parents /storage/.kodi/addons/.ups_power/bin/;
         mv --force ups /storage/.kodi/addons/.ups_power/bin/;
         chmod --changes u=rx,g=rx,o=rx /storage/.kodi/addons/.ups_power/;
   
         sed --regexp-extended --expression="s:ExecStart=\/bin\/ups\/:ExecStart=\/storage\/\.kodi\/addons\/\.ups_power\/bin:" ups.service
         mv --force ups.service /storage/.config/system.d/;
+elif [ ]
+then
+
 else
         mv --force ups /bin/;
         mv --force ups.service /etc/systemd/system/;
