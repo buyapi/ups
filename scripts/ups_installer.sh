e51e2384388ab70008c77095edd91946741f879c
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2019-12-21 23:30:14 -0500

    extending command options

--- scripts/ups_installer.sh
+++ scripts/ups_installer.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 os=$(uname -o);
-declare -u os;
+declare -ru os;
 
-wget -nd https://raw.githubusercontent.com/buyapi/ups/master/scripts/ups;
-wget -nd https://raw.githubusercontent.com/buyapi/ups/master/scripts/ups.service;
+wget --non-verbose --tries=3 https://raw.githubusercontent.com/buyapi/ups/master/scripts/ups;
+wget --non-verbose --tries=3 https://raw.githubusercontent.com/buyapi/ups/master/scripts/ups.service;
 
-chmod -f u=rx,g=rx,o=r ups;
-chmod -f u=rx,g=rx,o=r ups.service;
+chmod --changes u=rx,g=rx,o=r ups;
+chmod --changes u=rx,g=rx,o=r ups.service;
 
-chown -f root:root ups;
-chown -f root:root ups.service;
+chown --changes root:root ups;
+chown --changes root:root ups.service;
 
 if [ "$os" = "LIBREELEC" ] || [ "$os" = "OPENELEC" ]
 then
         # these will run as normal, as the default user account is root
-        mkdir -p /storage/.kodi/addons/.ups_power/bin;
-        mv -f ups /storage/.kodi/addons/.ups_power/bin;
-        chmod u=rx,g=rx,o=rx /storage/.kodi/addons/.ups_power/;
+        mkdir --parents /storage/.kodi/addons/.ups_power/bin/;
+        mv --force ups /storage/.kodi/addons/.ups_power/bin/;
+        chmod --changes u=rx,g=rx,o=rx /storage/.kodi/addons/.ups_power/;
   
-        sed -e "s:ExecStart=\/bin\/ups\/:ExecStart=\/storage\/\.kodi\/addons\/\.ups_power\/bin:" ups.service
-        mv -f ups.service /storage/.config/system.d/;
+        sed --regexp-extended --expression="s:ExecStart=\/bin\/ups\/:ExecStart=\/storage\/\.kodi\/addons\/\.ups_power\/bin:" ups.service
+        mv --force ups.service /storage/.config/system.d/;
 else
-        mv -f ups /bin/;
-        mv -f ups.service /etc/systemd/system/;
+        mv --force ups /bin/;
+        mv --force ups.service /etc/systemd/system/;
 fi
